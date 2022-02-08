5e60880dcd5115537f7421fb7bdd85a28706393e
aon <21188659+aon@users.noreply.github.com>
2022-02-08 16:50:52 -0300

    Update

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,17 @@
+import {useContext} from "react";
+
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 function FavoritesPage() {
-  return <div>Favorites Page</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+  const anyFavorites = favoritesCtx.totalFavorites > 0
+
+  return <section>
+    <h1>My Favorites</h1>
+    {anyFavorites && <MeetupList meetups={favoritesCtx.favorites} />}
+    {!anyFavorites && <p>You have no favorites yet. Wanna add some?</p>}
+  </section>;
 }
 
 export default FavoritesPage;
